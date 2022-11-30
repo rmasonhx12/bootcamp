5abce06dda72aa0799f181187d7c2d2275d44ed1
rmasonhx12 <rmasonh22@gmail.com>
2022-11-29 19:29:31 -0500

    conditional logic -- v-if v-if-else v-else

--- project3/main.js
+++ project3/main.js
@@ -3,7 +3,10 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inStock: true
+            //inStock: true
+            //inventory: 0   // 0 = out of stock
+            //inventory: 5 // 5 = Almost sold out of stock
+            inventory: 100 // 100 = in stock
         }
     }
 })
