520cc33ce3a6b2e4971c4f6a6b57fa9ae61c49c8
rmasonhx12 <rmasonh22@gmail.com>
2022-11-29 19:33:41 -0500

    add on sale

--- project3/main.js
+++ project3/main.js
@@ -4,9 +4,10 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             //inStock: true
-            //inventory: 0   // 0 = out of stock
+           //inventory: 0   // 0 = out of stock
             //inventory: 5 // 5 = Almost sold out of stock
-            inventory: 100 // 100 = in stock
+            inventory: 100, // 100 = in stock
+            onSale: true
         }
     }
 })
