b38569d6be5cbcf13bcd92e6d3d3528bf7e16feb
rmasonhx12 <rmasonh22@gmail.com>
2022-11-30 10:56:57 -0500

    isActive class

--- project6/main.js
+++ project6/main.js
@@ -1,11 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
+            
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
             inStock: false,
+            isActive: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
